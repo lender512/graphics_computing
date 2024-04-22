8aae996d48343b8ce50b9cd839f55ff4a67c9441
lender512 <luis.berrospi@utec.edu.pe>
2024-04-21 20:12:54 -0500

    owo

---
+++ practicas/LuisAlfonsoBerrospiRodriguez-graded-practise-1/exercise04/solution.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2
+import math
+
+# Cambiar el brillo de una imagen
+
+img_path = 'lenna.png'
+final_img_path = 'exercise04/output/lenna-colorscale.png'
+
+def target(img, target_img):
+
+    img = img.astype(np.float32)
+    new_img = np.zeros((img.shape[0], img.shape[1], 3))
+    rates = np.max(img, axis=2)
+    rates /= 255
+    new_img = target_img * rates[:, :, None]
+    new_img = new_img.astype(np.uint8)
+    
+    return new_img
+
+img = cv2.imread(img_path)
+widh, height = img.shape[:2]
+target_img = np.zeros((widh, height, 3), np.uint8)
+center = (widh // 2, height // 2)
+radius = widh // 2
+for i in range(widh):
+    for j in range(height):
+        if math.sqrt((i - center[0])**2 + (j - center[1])**2) < radius:
+            target_img[i, j] = [255, 0, 0]
+
+
+img = target(img, target_img)
+cv2.imwrite(final_img_path, img)
